0cacb3a8418e13b356b5dee2db76cbb6b004b4ae
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-10-14 04:10:20 -0300

    fix card typing and conditional menu rendering

--- shared/components/card.tsx
+++ shared/components/card.tsx
@@ -3,29 +3,36 @@ import { MenuItem } from "primereact/menuitem"
 import { Menu } from "primereact/menu"
 import { Button } from "primereact/button"
 import { PrimeIcons } from "primereact/api"
+import clsx from "clsx"
 
-export function Card({
-  title,
-  menu,
-  children,
-}: PropsWithChildren<{ title: string; menu?: MenuItem[] }>) {
+interface CardProps {
+  title: string
+  menu?: MenuItem[]
+}
+
+export function Card({ title, menu, children }: PropsWithChildren<CardProps>) {
   const menuRef = useRef<Menu>(null)
 
   return (
     <div className="bg-card border border-card-border p-5 rounded flex flex-col gap-2 transition-all hover:border-card-hover hover:-translate-y-1">
-      <header className="flex items-center justify-between">
+      <header
+        className={clsx("flex items-center", { "justify-between": !!menu })}
+      >
         <h2 className="font-bold">{title}</h2>
-        <Button
-          icon={PrimeIcons.ELLIPSIS_V}
-          onClick={(event) => {
-            event.preventDefault()
-            return menuRef.current?.toggle(event)
-          }}
-          aria-controls="popup_menu"
-          aria-haspopup
-          rounded
-          text
-        />
+
+        {menu && (
+          <Button
+            icon={PrimeIcons.ELLIPSIS_V}
+            onClick={(event) => {
+              event.preventDefault()
+              return menuRef.current?.toggle(event)
+            }}
+            aria-controls="popup_menu"
+            aria-haspopup
+            rounded
+            text
+          />
+        )}
       </header>
 
       {children && <div>{children}</div>}
